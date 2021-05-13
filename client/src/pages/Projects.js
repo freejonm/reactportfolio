7ad21b3740a1a61a841eea81aae006d56e63a818
Jon Free <freejonm@gmail.com>
2021-05-13 09:56:21 -0400

    adding projects page

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron';
+const Projects = () => {
+  return (
+    <div>
+      <main>
+        <Jumbotron  />
+        <section className="container-fluid gif-section">
+          <div className="row mt-0 mb-0">
+            
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Projects;
