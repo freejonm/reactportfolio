9dbd384400e131b292891ed0b5f0318ed5abd942
Jon Free <freejonm@gmail.com>
2021-05-13 11:01:42 -0400

    adding projects

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -53,6 +53,88 @@ const Projects = () => {
                   </p>
                 </div>
               </div>
+              <div className="col-md-4">
+              <div className="card h-100 mb-4">
+                <a
+                  href="https://github.com/freejonm/budgettracker"
+                  target="_blank"
+                >
+                  <img
+                    className="card-img-top active"
+                    // src="assets/Images/employee-trk-demo.gif"
+                    alt=""
+                  />{' '}
+                </a>
+                <div className="card-body">
+                  <h2 className="card-header">Budget Tracker</h2>
+                  <p className="card-text">
+                   
+                  {' '}
+                  An app that allows users to keep track of spending/budgets whether offline or online Deployed to heroku{' '}
+                    <a
+                      href="https://budget-tracker87.herokuapp.com/"
+                      className="text-secondary"
+                    >
+                      here
+                    </a>
+                    .
+        
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4">
+              <div className="card h-100 mb-4">
+                <a
+                  href="https://github.com/chrisrisseler/project2"
+                  target="_blank"
+                >
+                  <img
+                    className="card-img-top active"
+                    // src="assets/Images/employee-trk-demo.gif"
+                    alt=""
+                  />{' '}
+                </a>
+                <div className="card-body">
+                  <h2 className="card-header">Media Munch</h2>
+                  <p className="card-text">
+                   
+                  {' '}
+                  An app that uses OMDB and ComicsVine APIs to allow users to save reviews of multiple forms of media that they consume. Deployed to heroku{' '}
+                    <a
+                      href="https://media-munch.herokuapp.com/"
+                      className="text-secondary"
+                    >
+                      here
+                    </a>
+                    .
+        
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4">
+              <div className="card h-100 mb-4">
+                <a
+                  href="https://github.com/stanleymcoy/team5-project1"
+                  target="_blank"
+                >
+                  <img
+                    className="card-img-top active"
+                    // src="assets/Images/employee-trk-demo.gif"
+                    alt=""
+                  />{' '}
+                </a>
+                <div className="card-body">
+                  <h2 className="card-header">Playcation</h2>
+                  <p className="card-text">
+                   
+                  {' '}
+                  An app that uses Open Weather Map, Pixabay, and LastFM APIs to allow users to take virtual vacations to the location of their choice.
+                  </p>
+                </div>
+              </div>
+            </div>
+            </div>
+            </div>
             </div>
             </div>
             </div>
