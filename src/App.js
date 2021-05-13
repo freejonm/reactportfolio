16b291c87a0f3742aa2dad22980a627490f400c0
Jon Free <freejonm@gmail.com>
2021-05-13 18:23:17 -0400

    changed router basename

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename='/reactportfolio'>
           <Route exact path="/" component={Home} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/contact" component={Contact} />
